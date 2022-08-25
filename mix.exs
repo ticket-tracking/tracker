9cb1df7adbbd7a8a92505d5fecec063ab2acda2b
Junaid Farooq <fjunaid252@gmail.com>
2022-08-25 14:53:50 +0500

    CI test.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Tracker.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.github": :test
       ]
     ]
   end
